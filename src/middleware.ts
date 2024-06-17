89433f52e11ffa18ece741de409a2e794e8a329e
Ahmad Hijazi <ahmadhijazi_me@hotmail.com>
2024-06-17 23:11:48 +0200

    fix prod

--- src/middleware.ts
+++ src/middleware.ts
@@ -33,6 +33,13 @@ export function middleware(req: any) {
 
 export const config = {
     matcher: [
-        '/((?!api|_next/static|_next/image|assets|images|favicon.ico|logo.png|sw.js).*)',
+        /*
+         * Match all request paths except for the ones starting with:
+         * - api (API routes)
+         * - _next/static (static files)
+         * - _next/image (image optimization files)
+         * - favicon.ico (favicon file)
+         */
+        '/((?!api|_next/static|_next/image|favicon.ico).*)',
     ],
-};
+}
